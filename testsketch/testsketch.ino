28807d7927ffa148cb9466fcd1643fdc73aa6424
Ryan Pavlik <rpavlik@iastate.edu>
2012-06-29 16:24:41 -0500

    Simpler test code that doesn't drag in all the serial stuff.

--- testsketch/testsketch.ino
+++ testsketch/testsketch.ino
@@ -11,18 +11,16 @@ Vector3f v1(1.5, 2.0, -.3);
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   delay(2000);
 
 }
 
 void loop() {
-  
-  Serial.println("Starting some math output");
-
   /// Print the norm (length) of vector v1
   float length = v1.norm();
-  Serial.println(length);
+  if (length < 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
